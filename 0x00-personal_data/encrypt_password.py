a38d3a5d16d1d029ea9d8781cad42790f6cfce9f
“Annah <njugunaannah@gmail.com>
2024-02-07 19:45:52 +0300

    encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,6 +7,31 @@ import os
 import mysql.connector
 from typing import List
 import logging
+import bcrypt
+from bcrypt import hashpw
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Returns a hashed password
+    Args:
+        password (str): password to be hashed
+    """
+    b = password.encode()
+    hashed = hashpw(b, bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check whether a password is valid
+    Args:
+        hashed_password (bytes): hashed password
+        password (str): password in string
+    Return:
+        bool
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
 
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
