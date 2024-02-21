b8a9ea708d491af3db4298576872037d76754eb3
“Annah <njugunaannah@gmail.com>
2024-02-21 20:08:22 +0300

    edits

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,50 +1,72 @@
 #!/usr/bin/env python3
-""" 3. Auth class
 """
-
+Definition of class Auth
+"""
+import os
 from flask import request
-from os import getenv
-from typing import List, TypeVar
+from typing import (
+    List,
+    TypeVar
+)
 
 
 class Auth:
-    """ Auth class.
     """
-
+    Manages the API authentication
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require_auth method
         """
-        if path is None or not excluded_paths:
+        Determines whether a given path requires authentication or not
+        Args:
+            - path(str): Url path to be checked
+            - excluded_paths(List of str): List of paths that do not require
+              authentication
+        Return:
+            - True if path is not in excluded_paths, else False
+        """
+        if path is None:
             return True
-        path = path + '/' if path[-1] != '/' else path
-        has_wildcard = any(x.endswith("*") for x in excluded_paths)
-        if not has_wildcard:
-            return path not in excluded_paths
-        for entry in excluded_paths:
-            if entry.endswith("*"):
-                if path.startswith(entry[:-1]):
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
                     return False
-            if path == entry:
-                return False
+                if path.startswith(i):
+                    return False
+                if i[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
         return True
 
     def authorization_header(self, request=None) -> str:
-        """ authorization_header method
+        """
+        Returns the authorization header from a request object
         """
         if request is None:
             return None
-        if 'Authorization' not in request.headers:
+        header = request.headers.get('Authorization')
+        if header is None:
             return None
-        return request.headers.get('Authorization')
+        return header
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ curent_user method
+        """
+        Returns a User instance from information from a request object
         """
         return None
 
     def session_cookie(self, request=None):
-        """ session_cookie method
         """
-        if request:
-            session_name = getenv("SESSION_NAME")
-            return request.cookies.get(session_name, None)
+        Returns a cookie from a request
+        Args:
+            request : request object
+        Return:
+            value of _my_session_id cookie from request object
+        """
+        if request is None:
+            return None
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)
--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,57 +1,66 @@
 #!/usr/bin/env python3
-""" SessionExpAuth module
 """
+Define SessionExpAuth class
+"""
+import os
+from datetime import (
+    datetime,
+    timedelta
+)
 
-from api.v1.auth.session_auth import SessionAuth
-from datetime import datetime, timedelta
-from os import getenv
+from .session_auth import SessionAuth
 
 
 class SessionExpAuth(SessionAuth):
-    """ SessionExpAuth class.
     """
-
+    Definition of class SessionExpAuth that adds an
+    expiration date to a Session ID
+    """
     def __init__(self):
-        """ Constructor.
         """
-        duration = getenv('SESSION_DURATION')
-        if duration:
-            try:
-                self.session_duration = int(duration)
-            except Exception:
-                self.session_duration = 0
-        else:
-            self.session_duration = 0
+        Initialize the class
+        """
+        try:
+            duration = int(os.getenv('SESSION_DURATION'))
+        except Exception:
+            duration = 0
+        self.session_duration = duration
 
     def create_session(self, user_id=None):
-        """ create_session.
         """
-        if user_id:
-            session_id = super().create_session(user_id)
-            if not session_id:
-                return
-            user_id = self.user_id_by_session_id.get(session_id)
-            if not user_id:
-                return
-            session_dict = {'user_id': user_id, 'created_at': datetime.now()}
-            self.user_id_by_session_id[session_id] = session_dict
-            return session_id
+        Create a Session ID for a user_id
+        Args:
+            user_id (str): user id
+        """
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        session_dictionary = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
+        self.user_id_by_session_id[session_id] = session_dictionary
+        return session_id
 
     def user_id_for_session_id(self, session_id=None):
-        """ user_id_for_session_id.
         """
-        if not session_id:
-            return
-        session_dict = self.user_id_by_session_id.get(session_id, None)
-        if session_dict:
-            user = session_dict.get('user_id', None)
-            if user:
-                sd = self.session_duration
-                if sd <= 0:
-                    return user
-                created_at = session_dict.get('created_at', None)
-                if not created_at:
-                    return
-                if datetime.now() > created_at + timedelta(seconds=sd):
-                    return
-                return user
+        Returns a user ID based on a session ID
+        Args:
+            session_id (str): session ID
+        Return:
+            user id or None if session_id is None or not a string
+        """
+        if session_id is None:
+            return None
+        user_details = self.user_id_by_session_id.get(session_id)
+        if user_details is None:
+            return None
+        if "created_at" not in user_details.keys():
+            return None
+        if self.session_duration <= 0:
+            return user_details.get("user_id")
+        created_at = user_details.get("created_at")
+        allowed_window = created_at + timedelta(seconds=self.session_duration)
+        if allowed_window < datetime.now():
+            return None
+        return user_details.get("user_id")
